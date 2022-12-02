0ebcba247233adcae3127768878391ca400320bb
Roni <ronniiii@yahoo.com>
2022-12-02 02:42:31 +0000

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
+a = ""
+for i in reversed(range(96, 123)):
+    print("{}".format(a), end="")
     if i % 2 == 0:
-        print(chr(i), end="")
+        a = chr(i)
     else:
-        print(chr(i - 32), end="")
+        a = chr(i - 32)
