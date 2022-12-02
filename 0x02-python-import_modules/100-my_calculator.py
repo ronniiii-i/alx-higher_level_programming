f6a8ca3586e15a8d29911473950165a037c5c98e
Roni <ronniiii@yahoo.com>
2022-12-02 00:49:31 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    argc = len(argv)
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    if argc > 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+    op = argv[2]
+    ops = {
+            "+": add(a, b),
+            "-": sub(a, b),
+            "*": mul(a, b),
+            "/": div(a, b)}
+    if op in ops.keys():
+        print("{:d} {} {:d} = {:d}".format(a, op, b, ops[op]))
     else:
-        if argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif argv[2] == "-":
-            print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-        elif argv[2] == "*":
-            print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-        elif argv[2] == "/":
-            print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
