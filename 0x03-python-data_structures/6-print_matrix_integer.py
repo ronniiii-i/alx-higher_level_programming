2b1296dc04dabe35feaeca734e074ea99e127787
Roni <ronniiii@yahoo.com>
2022-12-16 19:05:11 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         # Iterate over the elements in the row
         for element in row:
             # Print the element with the appropriate padding
-            print("{:d}".format(element), end=" ")
+            print("{:d}".format(element), end="")
+            if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
         # Print a newline after each row
         print()
