4828b764a3eea396fcb56bf2766448f35d18efff
Roni <ronniiii@yahoo.com>
2022-12-16 19:18:52 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
+    if len(sentence) < 1:
+        return length, None
     first = sentence[:1]
     return length, first
