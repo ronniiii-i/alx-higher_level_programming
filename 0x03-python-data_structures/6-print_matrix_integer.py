4de73e692768e0b561bf67e95bbfabad284ef5a3
Roni <ronniiii@yahoo.com>
2022-12-16 18:52:13 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,6 +9,3 @@ def print_matrix_integer(matrix=[[]]):
         for element in row:
             # Print the element with the appropriate padding
             print("{:d}".format(element), end=" ")
-
-        # Print a newline after each row
-        print()
