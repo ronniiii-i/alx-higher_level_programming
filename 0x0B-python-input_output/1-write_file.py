6561a3a4821dabcd9a1d5965c709dba46a33e4ec
Roni <ronniiii@yahoo.com>
2023-01-10 16:39:23 +0000

    task 1 corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Program that writes text to a file
+"""
+
+
 def write_file(filename="", text=""):
+    if filename == "":
+        return
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
     return len(text)
