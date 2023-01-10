6a691a1d49648ed25a3c109eae0c3cf470a0e7bf
Roni <ronniiii@yahoo.com>
2023-01-10 16:37:18 +0000

    task 0 corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+Program that prints a text file to stdout
+"""
+
+
 def read_file(filename=""):
+    """ prints file contents """
     if filename == "":
         return
-    with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
-    print(read_data)
+    with open(filename, "r") as f:
+        print(f.read(), end="")
