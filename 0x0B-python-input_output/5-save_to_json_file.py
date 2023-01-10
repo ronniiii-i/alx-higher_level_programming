208da881a1028e3b4dd825cd74ed9527ffe1e658
Roni <ronniiii@yahoo.com>
2023-01-10 16:59:57 +0000

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,5 @@ def save_to_json_file(my_obj, filename):
     if filename = "":
         return
     data = json.dumps(my_obj)
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write(data)
