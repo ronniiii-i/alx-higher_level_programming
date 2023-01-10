0e86480316dbf20e8977c612c370aa050c9e5ff1
Roni <ronniiii@yahoo.com>
2023-01-10 16:35:41 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    if filename == "":
+        return
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
