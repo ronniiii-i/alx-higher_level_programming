140623102f878e8d6aa09d983175f57fa676600f
Roni <ronniiii@yahoo.com>
2023-01-10 16:45:06 +0000

    task 1 corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ Program that writes text to a file
 
 
 def write_file(filename="", text=""):
+    """ write to a file and return length of characters written """
     if filename == "":
         return
     with open(filename, "w", encoding="utf-8") as f:
