2e1eb3aa18401cad502a6574b523f0366fbc0526
Roni <ronniiii@yahoo.com>
2023-01-26 21:50:24 +0000

    [TASK] Task 2: Print only integers in list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=" ")
+                print("{:d}".format(my_list[i]), end="")
                 elements_printed += 1
         except (ValueError, TypeError):
-            continue
+            break
     print()
     return elements_printed
