32634645be7a3887dabfe361e965ef8b45a43465
Roni <ronniiii@yahoo.com>
2023-01-26 21:07:57 +0000

    readme init

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elements_printed = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end=" ")
+            elements_printed += 1
+        except:
+            elements_printed -= 1
+    print()
+    return elements_printed
