e0c9a6ccfb4c143feef0f01e86326d301097645b
Roni <ronniiii@yahoo.com>
2023-01-26 21:13:42 +0000

    [TASK] Task 0: Print elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end=" ")
             elements_printed += 1
         except:
-            elements_printed -= 1
+            break
     print()
     return elements_printed
