7091e3c1b869eaece3ab8559cf262dfbc314c6d6
Roni <ronniiii@yahoo.com>
2023-01-27 21:20:04 +0000

    [FIX] Task 4: change val to 0 if error occurd

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    val = 0
     for i in range(list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
+                val = 0
                 raise IndexError("out of range")
             elif not (isinstance(my_list_1[i], (int, float)) and
                       isinstance(my_list_2[i], (int, float))):
+                val = 0
                 raise TypeError("wrong type")
             elif my_list_2[i] == 0:
+                val = 0
                 raise ZeroDivisionError("division by 0")
             else:
                 val = my_list_1[i] / my_list_2[i]
