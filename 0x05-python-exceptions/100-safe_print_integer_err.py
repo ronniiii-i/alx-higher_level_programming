0d528b1bc8526b49b8ca8e324ab0d90b0bc69150
Roni <ronniiii@yahoo.com>
2023-01-27 22:03:11 +0000

    [FIX] Task 7 (advanced): print to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as e:
-        print(e)
+        print(e, file=sys.stderr)
         return False
