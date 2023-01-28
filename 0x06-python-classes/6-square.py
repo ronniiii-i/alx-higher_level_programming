981597aed30039beee7cc8cf6b23251ca6343481
Roni <ronniiii@yahoo.com>
2023-01-28 22:01:29 +0000

    [FIX] Task 6: cover all possibilities

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,7 +71,8 @@ class Square:
         """
         if not isinstance(value, tuple)\
                 or len(value) != 2\
-                or (value[0] or value[1]) < 0:
+                or (value[0] or value[1]) < 0\
+                or not isinstance(value, int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
