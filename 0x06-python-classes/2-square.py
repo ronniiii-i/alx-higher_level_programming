f889109db0054a770dc80f2b52c270216edab822
Roni <ronniiii@yahoo.com>
2023-01-28 16:13:10 +0000

    [FIX] Task 2: change instance to just integer

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        if not isinstance(size, (int, float)):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
