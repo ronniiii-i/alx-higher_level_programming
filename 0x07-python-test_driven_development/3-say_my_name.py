d3753e719da54cf468188477f8b4165a1a85123a
Roni <ronniiii@yahoo.com>
2023-03-09 22:27:07 +0000

    remove test values in py file and added test file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,13 +18,3 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("Hello {} {}".format(first_name, last_name))
-    # print(f"Hello {first_name} {last_name}")
-
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
