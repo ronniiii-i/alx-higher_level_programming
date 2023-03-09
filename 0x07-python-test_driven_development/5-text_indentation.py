61625495be1b074944d39303bb5e94e895289df7
Roni <ronniiii@yahoo.com>
2023-03-09 23:58:10 +0000

    [FIX]fix error in python code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,11 +13,11 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    print(text.replace(". ", ".\n\n")
-          .replace(".", "?\n\n")
-          .replace("? ", "?\n\n")
-          .replace("?", "?\n\n")
-          .replace(", ", ",\n\n")
-          .replace(",", ",\n\n")
-          .replace(": ", ":\n\n")
-          .replace(":", ":\n\n"))
+    newText = ""
+    for i in range(len(text)):
+        if not i == 0 and text[i] == " " and text[i - 1] in ".?:":
+            continue
+        newText += text[i]
+        if text[i] in ".?:":
+            newText += "\n\n"
+    print(newText, end="")
