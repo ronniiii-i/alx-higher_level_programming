cd9a9b9053f9c10b6ee9ba75e7657a53a29f76cf
Roni <ronniiii@yahoo.com>
2023-03-10 02:13:41 +0000

    [FIX]pycodestyle validation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,7 +4,7 @@
 
 class LockedClass:
     """
-    The LockedClass is a class that prevents the user from dynamically 
+    The LockedClass is a class that prevents the user from dynamically
      creating new instance attributes,
     except if the new instance attribute is called first_name.
     """
