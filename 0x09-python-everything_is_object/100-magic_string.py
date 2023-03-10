b59e55fbc9b5dbaeb24361e05d479c138dc92611
Roni <ronniiii@yahoo.com>
2023-03-10 01:44:11 +0000

    [FIX]Remove documentation

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-"""Define a function that strings BestSchool n time"""
-
-
 def magic_string():
-    """Return a string "BestSchool" n times the number of the iteration
-
-    Returns:
-    A string with the format "BestSchool, BestSchool, ..., BestSchool"
-    with n-1 commas and n "BestSchool" strings, where n is the number
-    of times the function has been called.
-    """
     magic_string.count = getattr(magic_string, 'count', -1) + 1
     return "BestSchool" + ", BestSchool" * magic_string.count
