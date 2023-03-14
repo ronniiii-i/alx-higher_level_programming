521014c56a7a6f97a6edc369b76c097767ea8647
Roni <ronniiii@yahoo.com>
2023-03-14 22:32:41 +0000

    [FIX]Match semistandard rules

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args[0]) {
-    for (let i = 0; i < args[0]; i++) {
-        console.log("C is fun");
-    }
+  for (let i = 0; i < args[0]; i++) {
+    console.log('C is fun');
+  }
 } else {
-    console.log("Missing number of occurences");
+  console.log('Missing number of occurences');
 }
