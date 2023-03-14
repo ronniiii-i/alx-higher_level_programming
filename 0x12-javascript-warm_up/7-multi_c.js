d7b57f8fae48769fd4d03bb074d3a78d5f50563a
Roni <ronniiii@yahoo.com>
2023-03-14 22:31:46 +0000

    [NEW]Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args[0]) {
+    for (let i = 0; i < args[0]; i++) {
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing number of occurences");
+}
