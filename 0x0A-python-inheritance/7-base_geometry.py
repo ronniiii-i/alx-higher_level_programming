2ee3b6c70b0e982cfafe6fdf61c6cbababc1390d
Roni <ronniiii@yahoo.com>
2023-03-15 22:42:56 +0000

    [NEW]Python file for task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class BaseGeometry:
+    """
+    Defines a class called BaseGeometry
+    """
+    def area(self):
+        """
+        raises Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value
+        Arg:
+            name(str): name of the variable
+            value(int): value to be validated
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
