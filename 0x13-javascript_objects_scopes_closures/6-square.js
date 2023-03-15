9010146f84ed4059dcb75626573c987ab4720e9b
Roni <ronniiii@yahoo.com>
2023-03-15 00:20:55 +0000

    [NEW]Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size);
+  }
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = ""
+      for (let j = 0; j < this.width; j++) {
+        row += c
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
