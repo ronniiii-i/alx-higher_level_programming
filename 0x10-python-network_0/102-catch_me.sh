592c759c6bacd24ecc681656abc49b96a408ff7a
Roni <ronniiii@yahoo.com>
2023-03-31 22:26:50 +0000

    [FIX]fix code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a req to a url for a response
-curl -sLX PUT -d "user_id=1" -H "Origin: ALX School" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
