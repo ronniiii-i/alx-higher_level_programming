28b8a3c48185d4700e983f21577dc7223594436b
Roni <ronniiii@yahoo.com>
2023-04-01 02:19:10 +0000

    [FIX]remove unnecessary code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     email = {"email": argv[2]}
     data = urllib.parse.urlencode(email).encode('utf-8')
     with urllib.request.urlopen(argv[1], data) as response:
-        print("Your email is: {}".format(response.read().decode()))
+        print(response.read().decode())
