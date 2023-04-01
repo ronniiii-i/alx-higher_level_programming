b85757aada0c8144dd967600998b5a228b55d73f
Roni <ronniiii@yahoo.com>
2023-04-01 20:04:34 +0000

    [FIX]fix code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    if len(argv) < 1:
+    if len(argv) < 2:
         params = {"q": ""}
     else:
         params = {"q": argv[1]}
