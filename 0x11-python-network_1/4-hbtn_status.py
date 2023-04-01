032f6eafda3ae58d22133ebe7133965377b015db
Roni <ronniiii@yahoo.com>
2023-04-01 03:55:15 +0000

    [FIX]correct code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,9 @@
 import requests
 
 
-r = requests.get("https://alx-intranet.hbtn.io/status")
-# print(r.content)
-print("Body response:")
-print("\t- type: {}".format(type(r)))
-print("\t- content: {}".format(r.content))
+if __name__ == '__main__':
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    # print(r.content)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
