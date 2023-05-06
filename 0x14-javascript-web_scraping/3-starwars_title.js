927d5a1e84db69de39af51fd1906dca0d4019b15
Roni <ronniiii@yahoo.com>
2023-05-06 01:18:34 +0000

    [FIX]attempt #1

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-// const request = require('request');
-// request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`)
-//   .on('response', function (response) {
-//     console.log(`code: ${response.statusCode}`);
-//   });
-
-const https = require('https');
-https.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, res => {
-  // console.log('Status Code:', res.statusCode);
-  res.on('data', (d) => {
+const request = require('request');
+request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`)
+  .on('data', (d) => {
     console.log(d);
   });
-//   console.log(data);
-});
