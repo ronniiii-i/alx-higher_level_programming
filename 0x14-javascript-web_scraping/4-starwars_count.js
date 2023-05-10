617da601b4e3652adfc55c4fb6a89b6452746090
Roni <ronniiiip@gmail.com>
2023-05-10 20:17:15 +0000

    [REDO]Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/', function (error, response, body) {
-  console.log(error || JSON.parse(body).count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
 });
