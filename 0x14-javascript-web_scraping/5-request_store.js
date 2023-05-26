c240cd57063f8162da9c983e5093b7a4dcc53938
Roni <ronniiii@yahoo.com>
2023-05-26 18:30:09 +0000

    [UPDATE]Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-const request = require('request')
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
