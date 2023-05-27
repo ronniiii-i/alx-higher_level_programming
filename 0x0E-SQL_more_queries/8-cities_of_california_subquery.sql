2375feabf8a2249a3ff05e785c15bd1d084cef60
Roni <ronniiiip@gmail.com>
2023-05-27 12:10:33 -0700

    [NEW]Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- list cities in Carlifonia in the db
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+ORDER BY cities.id ASC;
