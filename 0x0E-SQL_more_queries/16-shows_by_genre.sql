fc51791c1b221c3f83bc51ae29c8112fb7ef0449
Roni <ronniiiip@gmail.com>
2023-06-09 15:33:08 -0700

    [FIX]Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,9 @@
 -- lists all shows, and all genres linked to that show, from the database
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
