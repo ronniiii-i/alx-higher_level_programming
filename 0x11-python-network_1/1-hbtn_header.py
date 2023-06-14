c67fd5a0d32933adb7f681ec90502ad686de7fd3
Roni <ronniiii@yahoo.com>
2023-06-14 21:49:47 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Python script that sends a req to a url and displays a header value"""
-import urllib.request
-from sys import argv
-
-
-if __name__ == '__main__':
-    with urllib.request.urlopen(argv[1]) as response:
-        print(response.headers.get("X-Request-Id"))
