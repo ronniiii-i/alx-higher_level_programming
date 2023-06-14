67dd47f6b4464b1ae912ed280804134361086d0f
Roni <ronniiii@yahoo.com>
2023-06-14 21:50:17 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const file = fs.readFileSync(process.argv[2], 'utf8');
-console.log(file);
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
