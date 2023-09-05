d913aa7e4e97eedaa1d83b02f3905f1d041051ee
Roni <ronniiii@yahoo.com>
2023-09-05 21:12:27 +0000

    [NEW]Task 1: Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """
+    class that defines a rectangle by private instance attributes:
+        width and height
+    """
+    # how to define private instance attribute with property setter?
+    def __init__(self, width=0, height=0):
+        """Initialize Variables"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Returns width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
