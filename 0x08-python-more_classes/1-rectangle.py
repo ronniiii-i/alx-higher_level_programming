651ea902db2608c2ff9a2d05e905a10ca5725119
Roni <ronniiii@yahoo.com>
2023-09-05 21:21:11 +0000

    [UPDATE]fix bugs

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     # how to define private instance attribute with property setter?
     def __init__(self, width=0, height=0):
         """Initialize Variables"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -21,9 +21,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets width value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,8 +35,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets height value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
