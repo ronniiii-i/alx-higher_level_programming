01afa1d5547c8c91b63a104cfb8482c9c3667cc0
Roni <ronniiii@yahoo.com>
2023-09-05 21:31:39 +0000

    [NEW]Task 2: Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """
+    class that defines a rectangle by private instance attributes:
+        width and height
+    """
+    # how to define private instance attribute with property setter?
+    def __init__(self, width=0, height=0):
+        """Initialize Variables"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Returns width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """method to det area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """method to get perimiter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((2*self.__width)+(2*self.__height))
