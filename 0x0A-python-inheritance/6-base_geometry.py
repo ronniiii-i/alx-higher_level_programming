ca390c3c8905e2295e9e216e9c2afbf5cc9c5805
Roni <ronniiii@yahoo.com>
2023-09-11 17:20:58 +0000

    [NEW]Task 6 - Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class BaseGeometry:
+    """
+    Defines a class called BaseGeometry
+    """
+
+    def area(self):
+        """
+        raises Exception
+        """
+        raise Exception("area() is not implemented")
