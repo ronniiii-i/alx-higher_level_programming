a5c5cecc01722d457e91a3067cfce46be7403477
Roni <ronniiii@yahoo.com>
2023-09-19 23:19:03 +0000

    [UPDATE]Task 2 - First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,33 +22,81 @@ class Rectangle(Base):
             id (int): The identity of the new Rectangle.
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     # Getter methods
     def get_width(self):
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     def get_height(self):
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     def get_x(self):
+        """
+        Get the x-coordinate of the rectangle.
+
+        Returns:
+            int: The x-coordinate of the rectangle.
+        """
         return self.__x
 
     def get_y(self):
+        """
+        Get the y-coordinate of the rectangle.
+
+        Returns:
+            int: The y-coordinate of the rectangle.
+        """
         return self.__y
 
     # Setter methods
     def set_width(self, width):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            width (int): The new width value.
+        """
         self.__width = width
 
     def set_height(self, height):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            height (int): The new height value.
+        """
         self.__height = height
 
     def set_x(self, x):
+        """
+        Set the x-coordinate of the rectangle.
+
+        Args:
+            x (int): The new x-coordinate value.
+        """
         self.__x = x
 
     def set_y(self, y):
+        """
+        Set the y-coordinate of the rectangle.
+
+        Args:
+            y (int): The new y-coordinate value.
+        """
         self.__y = y
