d77ea09cda2592551862d1f499c6b486d61bec16
Roni <ronniiii@yahoo.com>
2023-09-19 23:20:11 +0000

    [UPDATE]Task 2 - First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,10 +22,10 @@ class Rectangle(Base):
             id (int): The identity of the new Rectangle.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     # Getter methods
     def get_width(self):
