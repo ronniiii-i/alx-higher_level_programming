bc31df5ccb0bd682d64ab126b19bfc4ef69c219b
Roni <ronniiii@yahoo.com>
2023-09-23 22:35:37 +0000

    [NEW]Task 10 - And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Defines a Square class that inherits from Rectangle.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new Square instance.
+        Args:
+            size (int): The size of the new Square (both width and height).
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Return a custom string representation of the Square.
+
+        Returns:
+            str: A string in custom format
+        """
+        return "[Square] ({}) {}/{} - {}".\
+            format(self.id, self.x, self.y, self.width)
